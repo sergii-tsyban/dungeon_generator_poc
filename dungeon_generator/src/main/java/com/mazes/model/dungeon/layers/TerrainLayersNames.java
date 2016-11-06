95b850e4f05e1d4d51d3facb7db21203504a8a8e
sergii.tsyban <sergii.tsyban@gmail.com>
2016-11-06 14:55:15 +0100

    add terrain layer controller stub

---
+++ dungeon_generator/src/main/java/com/mazes/model/dungeon/layers/TerrainLayersNames.java
@@ -0,0 +1,9 @@
+package com.mazes.model.dungeon.layers;
+
+public class TerrainLayersNames {
+
+    public static final String FLOOR_LAYER = "FLOOR"; // should be below player
+    public static final String WALLS_ON_TOP = "WALLS_T"; // should be below player
+    public static final String WALLS_ON_BOTTOM = "WALLS_B"; // should be above player
+
+}
---
+++ dungeon_generator/src/main/java/com/mazes/model/dungeon/layers/TileLayersController.java
@@ -0,0 +1,21 @@
+package com.mazes.model.dungeon.layers;
+
+public class TileLayersController {
+
+    private int[][] tiles;
+
+    public TileLayersController(int[][] tiles) {
+        this.tiles = tiles;
+    }
+
+    public void addLayer(String name, int priority, int[] tileIds){
+        // TBD
+    }
+
+    private class Layer{
+
+        int[] tilesIds;
+
+    }
+
+}
