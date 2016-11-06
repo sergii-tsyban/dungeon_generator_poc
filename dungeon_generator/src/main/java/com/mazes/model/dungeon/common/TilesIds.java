ef414bf11a5ad957ad478d5cfba55c4ad423b4c9
sergii.tsyban <sergii.tsyban@gmail.com>
2016-11-06 14:55:56 +0100

    add terrain tie ids constants

---
+++ dungeon_generator/src/main/java/com/mazes/model/dungeon/common/TilesIds.java
@@ -0,0 +1,22 @@
+package com.mazes.model.dungeon.common;
+
+/**
+ * Created by serg on 06/11/2016.
+ */
+public class TilesIds {
+
+    public static final int DARK = -1;
+
+    public static final int WALL_SOLID = 1;
+    public static final int FLOOR = 0;
+
+    public static final int WALL_R = 11;
+    public static final int WALL_L = 12;
+    public static final int WALL_T = 13;
+    public static final int WALL_B = 14;
+
+    public static final int CORNER_TR = 21;
+    public static final int CORNER_TL = 22;
+    public static final int CORNER_BR = 23;
+    public static final int CORNER_BL = 24;
+}
